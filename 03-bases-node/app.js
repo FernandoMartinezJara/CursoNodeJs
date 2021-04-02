68d3d5c970cd25184d7e24cb9b5b3d736456daa9
Fernando Martínez <frmartinezj@achs.cl>
2021-04-01 22:54:57 -0300

    bases node 1

---
+++ 03-bases-node/app.js
@@ -0,0 +1,6 @@
+console.clear();
+console.log('============ Tabla del 5 ============');
+console.log('=====================================');
+for (let index = 1; index < 11; index++) {
+    console.log(`5 x ${index} = ${ index * 5 }`);
+}
